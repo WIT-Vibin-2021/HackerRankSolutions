5e8e9aebe00bd7728c1e3ea7c99803ffe1bdec55
Vibin.V <mailme.vibin@gmail.com>
2022-01-15 15:48:44 +0000

    Create Cloud Jump.cs

---
+++ Cloud Jump.cs
@@ -0,0 +1,70 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<int> c)
+    {
+        int count = 0, i =0;
+        while(i<c.Count-1)
+        {                                                                         
+            if (c[i] == 0)  
+                i++;
+                
+            count++;
+            i++;
+        }
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> c = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(cTemp => Convert.ToInt32(cTemp)).ToList();
+
+        int result = Result.jumpingOnClouds(c);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
+/*------------------------------------------------
+7
+0 0 1 0 0 1 0
+Sample Output 0
+
+4
+Explanation 0:
+The player must avoid C[2] and C[5]. The game can be won with a minimum of 4 jumps:
+
+jump(2).png
+*/
